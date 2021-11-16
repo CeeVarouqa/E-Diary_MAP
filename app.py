20baa35218683217dbd5c753bd46546712552e53
alwa0 <a.paukova@innopolis.university>
2021-11-16 21:43:01 +0300

    add delete note

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app.config.from_pyfile('config.py')
 posts = [
     {
         'title': 'Note 1',
-        'content': 'First note',
-        'date_posted': '5 November, 2021'
+        'content': 'This is my first note. I wrote it on 10th of October, 2021. It is a sample note. '
+                   'It contains no meaning, but shows how my notes will look like. The end.',
+        'date_posted': '2021-10-10'
     },
     {
         'title': 'Note 2',
         'content': 'Second note',
-        'date_posted': '7 November, 2021'
+        'date_posted': '2021-10-15'
     }
 ]
 
@@ -30,7 +31,25 @@ def add_note():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        posts.append({'title': title, 'content': content, 'date_posted': datetime.datetime.now().date()})
+        titles = []
+        for post in posts:
+            titles.append(post["title"])
+        if title in titles:
+            return render_template('note.html', error="Note with such title already exists")
+        if title == "":
+            return render_template('note.html', error="You should fill the title field")
+        posts.append({'id': len(posts), 'title': title, 'content': content, 'date_posted': datetime.datetime.now().date()})
         return render_template('home.html', posts=posts)
     return render_template('note.html')
+
+
+@app.route('/delete_note', methods=["POST"])
+def delete_note():
+    if request.method == 'POST':
+        title = request.form['title']
+        for post in posts:
+            if post["title"] == title:
+                posts.remove(post)
+                break
+        return render_template('home.html', posts=posts)
 
