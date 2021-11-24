04b7ce1850e99fca1b154bc26e7a0628b7aee372
temurbekkhujaev <temurbekkhujaev@gmail.com>
2021-11-24 20:34:39 +0300

    photo upload

--- run.py
+++ run.py
@@ -2,8 +2,16 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from app import app, resources, models, db
 from flask import render_template
+from flask import request
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
 api = Api(app)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
 
 # map urls with functions
 api.add_resource(resources.UserRegistration, '/registration')
@@ -21,6 +29,11 @@ app.config['SECRET_KEY'] = 'SoftwareArchitecture'
 app.config['JWT_SECRET_KEY'] = 'kjgh234jht4h5hgkh'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = "secret key"
 
 # work with tokens
 jwt = JWTManager(app)
@@ -40,6 +53,40 @@ def create_tables():
 @app.route('/api/docs')
 def get_docs():
     return render_template('swaggerui.html')
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 if __name__ == '__main__':
