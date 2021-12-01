efe0fc44e9ce61fd3e7533fbd7e7338748dbb610
CeeVarouqa <CeeVarouqa@users.noreply.github.com>
2021-12-01 16:39:54 +0000

    autopep8 action fixes

--- posts.py
+++ posts.py
@@ -2,6 +2,14 @@ import datetime
 from typing import List
 
 
-def add_post(posts: List[dict], id: int, title: str, content: str, date_posted: datetime.datetime.date):
-    posts.append({'id': id, 'title': title, 'content': content, 'date_posted': date_posted})
+def add_post(
+        posts: List[dict],
+        id: int,
+        title: str,
+        content: str,
+        date_posted: datetime.datetime.date):
+    posts.append({'id': id,
+                  'title': title,
+                  'content': content,
+                  'date_posted': date_posted})
     return posts
